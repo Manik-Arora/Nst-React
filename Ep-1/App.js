39fcffc49221594f06135addfbdc83dd1b83cc0d
Manik-Arora <manikarora2605@gmail.com>
2024-04-11 14:52:17 +0530

    Ep-1

---
+++ Ep-1/App.js
@@ -0,0 +1,21 @@
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child1" }, [
+    React.createElement("h1", {}, "I'm a h1 tag!"),
+    React.createElement("h2", {}, "I'm a h2 tag!"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "I'm a h1 tag!"),
+    React.createElement("h2", {}, "I'm a h2 tag!"),
+  ]),
+]);
+
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "Hello World from React!!!"
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+//replaces whatever is inside div-root with parent
+root.render(parent);
